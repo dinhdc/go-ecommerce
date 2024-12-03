7f3484177c67de154b7619a3f43170dd0d0c7d79
Aino Dinh <congdinhpro2k@gmail.com>
2024-12-03 14:29:29 +0700

    update func logger

--- cmd/cli/main.log.go
+++ cmd/cli/main.log.go
@@ -27,7 +27,7 @@ func getEncoderLog() zapcore.Encoder {
 }
 
 func getWriteSync() zapcore.WriteSyncer {
-	file, _ := os.OpenFile("./log/log.txt", os.O_RDWR, os.ModePerm)
+	file, _ := os.OpenFile("./log/log.txt", os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	syncFile := zapcore.AddSync(file)
 	syncConsole := zapcore.AddSync(os.Stderr)
 	return zapcore.NewMultiWriteSyncer(syncConsole, syncFile)
